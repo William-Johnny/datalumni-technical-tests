e95eff21aa72ae1220b48f6b9430cbbe5c3501dd
William-Johnny <wjguenon@icloud.com>
2024-02-19 17:10:52 +0100

    completed number.py

--- python/number.py
+++ python/number.py
@@ -1,5 +1,22 @@
-# Your code goes here
+def numberSeeker ():
+    addition=0
+    listeNb = []
+    for n in range (1,1000):
+        nombre_str = str(n)
+        for chiffre in nombre_str:
+            listeNb.append(int(chiffre))
+        if len(listeNb)>=2:
+            if 1 not in listeNb and 7 not in listeNb:
+                for n in range (len(listeNb)):
+                    addition+=listeNb[n]
+                if addition<=10:
+                    if (listeNb[0]+listeNb[1])%2!=0 and listeNb[len(listeNb)-2]==4 and listeNb[len(listeNb)-1]==len(listeNb):
+                        result = ''.join(map(str, listeNb))
+                        return result
 
-mystery_number = # TODO
+        listeNb=[]
+        addition=0
 
-print(f'Le nombre mystère est le : {mystery_number}')
+mystery_number = numberSeeker ()
+
+print('Le nombre mystère est le :',mystery_number)
